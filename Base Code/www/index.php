6bc1cf16a28f7771f352db30627073b8f024d038
Tenebrisse <quentinhermiteau@gmail.com>
2018-11-02 15:08:42 +0100

    add index.php

--- Base Code/www/index.php
+++ Base Code/www/index.php
@@ -1,1 +1,46 @@
-<h1>Welcome !</h1>
+<?php
+
+spl_autoload_register(function($class) {
+    $classPath = 'core/' . $class . '.class.php';
+    if(file_exists($classPath)) {
+        include($classPath);
+    }
+});
+
+$slug = $_SERVER['REQUEST_URI'];
+
+/*
+    Différencier le controller de l'action
+    donc on se retrouve avec $c et $a
+    vérifier l'existence du fichier et de la class controller
+    instancier le controller
+    vérifier que la méthode (l'action) existe
+    appel de la méthode
+*/
+$slug = explode('?', $slug)[0];
+
+require 'core/Routing.class.php';
+
+$cap = Routing::getRoute($slug);
+extract($cap);
+
+if(file_exists($controllerPath)) {
+    include $controllerPath;
+    
+    if(class_exists($controller)) {
+        $object = new $controller();
+        
+        if(method_exists($object, $action)) {
+            $object->$action();
+
+            $slug = Routing::getSlug($controller, $action);
+        } else {
+            die('The method "' . $action . '" doesn\'t exist');
+        }
+    } else {
+        die('The class "' . $controller . '" doesn\'t exist');
+    }
+    
+} else {
+    die('Controller file "' . $controller . '" doesn\'t exist');
+}
