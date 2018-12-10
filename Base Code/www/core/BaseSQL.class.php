09764a4126097bdf163122079a732207c2fdd2bb
quentinhermiteau <quentinhermiteau@gmail.com>
2018-12-10 16:59:28 +0100

    fix save method

--- Base Code/www/core/BaseSQL.class.php
+++ Base Code/www/core/BaseSQL.class.php
@@ -3,63 +3,36 @@
 class BaseSQL {
 
     private $pdo;
+    private $table;
 
     public function __construct() {
         try {
-            $this->pdo = new PDO(DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PWD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+            $this->pdo = new PDO(DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PWD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         } catch(PDOException $e) {
             throw new Exception($e->getMessage());
         }
+
+        $this->table = get_called_class();
     }
 
     public function save() {
-        $tableName = get_called_class();
+        $dataObject = get_object_vars($this);
+        $dataChild = array_diff_key($dataObject, get_class_vars(get_class()));
 
-        $objectAttributes = get_object_vars($this);
-
-        $class = get_class();
-        $classAttributes = get_class_vars($class);
-
-        $attributes = array_diff_key($objectAttributes, $classAttributes);
-
-        $keys = [];
-        $values = [];
-        $stringKeys = '';
-        $query = '';
-
-        foreach($attributes as $key => $value) {
-            if(is_null($value)) {
-                continue;
-            }
-
-            $keys[] = $key;
-            $values[':' . $key] = $value;
-        }
-        
-        if(is_null($attributes['id'])) {
-            $stringKeys = implode(',', $keys);
-
-            $query = 'INSERT INTO ' . $tableName . '(' . $stringKeys . ') VALUES (';
-            
-            foreach($keys as $key) {
-                $query .= ':' . $key . ', ';
-            }
-            
-            $query = rtrim($query, ', ');
-            $query .= ');';
+        if (is_null($dataChild['id'])) {
+            $sql = 'INSERT INTO ' . $this->table . ' (' . implode(',', array_keys($dataChild)) . ') 
+            VALUES (:' . implode(',:', array_keys($dataChild)) .  ');';
         } else {
-            $query = 'UPDATE ' . $tableName . ' SET ';
-
-            foreach($keys as $key) {
-                $query .= $key . ' = :' . $key . ', ';
+            foreach($dataChild as $key => $value) {
+                if ($key != 'id') {
+                    $sqlUpdate[] = $key . '=:' . $key;
+                }
             }
-            $query = rtrim($query, ', ');
-            $query .= ' WHERE id = :id;';
+            
+            $sql = 'UPDATE ' . $this->table . ' SET ' . implode(',', $sqlUpdate) . ' WHERE id=:id';
         }
 
-        $statement = $this->pdo->prepare($query);
-        $statement->execute($values);
-
-        return $statement;
+        $query = $this->pdo->prepare($sql);
+        $query->execute($dataChild);
     }
 }
