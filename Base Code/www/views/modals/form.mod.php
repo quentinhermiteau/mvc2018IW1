105dd2ecf073e38eb66589a0247b13f31647e527
quentinhermiteau <quentinhermiteau@gmail.com>
2018-12-12 12:13:53 +0100

    new modal for form

---
+++ Base Code/www/views/modals/form.mod.php
@@ -0,0 +1,6 @@
+<form 
+    action="<?php echo $config['config']['action'] ?>" 
+    method="<?php echo $config['config']['method'] ?>"
+    class="<?php echo $config['config']['class'] ?>"
+    id="<?php echo $config['config']['id'] ?>">
+</form>
