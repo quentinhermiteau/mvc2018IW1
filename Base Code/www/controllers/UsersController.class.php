5a2f346e6104851d73cdf9e22d1e9806633019e4
quentinhermiteau <quentinhermiteau@gmail.com>
2019-03-08 23:15:43 +0100

    usage validator

--- Base Code/www/controllers/UsersController.class.php
+++ Base Code/www/controllers/UsersController.class.php
@@ -17,6 +17,23 @@ class UsersController {
         $user = new Users();
         $form = $user->getRegisterForm();
 
+        $method = strtoupper($form['config']['method']);
+        $data = $GLOBALS['_' . $method];
+
+        if ($_SERVER['REQUEST_METHOD'] === $method && !empty($data)) {
+            $validator = new Validator($form, $data);
+
+            if (empty($validator->errors)) {
+                $user->setFirstname($data['firstname']);
+                $user->setLastname($data['lastname']);
+                $user->setEmail($data['email']);
+                $user->setPwd($data['pwd']);
+                $user->save();
+            } else {
+
+            }
+        }
+
         $view = new View('addUser', 'front');
         $view->assign('form', $form);
     }
